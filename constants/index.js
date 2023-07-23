c19b8fe1c138a68671c4c94562d42bbc99c8b6ca
Hejartech <119725221+Hejartech@users.noreply.github.com>
2023-07-23 14:59:18 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x143615719e692dd21dfce8fdb662329c24fc8ba2";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
